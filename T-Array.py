5de991ddee34d9d93c708f4d6a3189b05a586032
DavaIM105 <l200220105@student.ums.ac.id>
2024-03-15 21:10:51 +0700

    first code

---
+++ T-Array.py
@@ -0,0 +1,153 @@
+# nama_satelit = [
+#     "Hubble Space Telescope",
+#     "International Space Station (ISS)",
+#     "Tiangong Space Station",
+#     "Vanguard 1",
+#     "Explorer 1",
+#     "Sputnik 1",
+#     "Sputnik 2",
+#     "Telstar",
+#     "Skynet 1",
+#     "Skynet 2",
+#     "Landsat 1",
+#     "Landsat 2",
+#     "Landsat 3",
+#     "Voyager 1",
+#     "Voyager 2",
+#     "Galileo",
+#     "Chandra X-ray Observatory",
+#     "Kepler Space Telescope",
+#     "James Webb Space Telescope",
+#     "Hubble Space Telescope",
+#     "Spitzer Space Telescope",
+#     "Compton Gamma Ray Observatory",
+#     "Swift Gamma-Ray Burst Mission",
+#     "Fermi Gamma-ray Space Telescope",
+#     "Planck Space Observatory",
+#     "Herschel Space Observatory",
+#     "Gaia",
+#     "Mars Reconnaissance Orbiter",
+#     "Cassini-Huygens",
+#     "New Horizons",
+#     "Juno",
+#     "Chang'e 4",
+#     "Chang'e 5",
+#     "Chang'e 6",
+#     "Lunar Reconnaissance Orbiter",
+#     "Lunar Orbiter 1",
+#     "Lunar Orbiter 2",
+#     "Lunar Orbiter 3",
+#     "Lunar Orbiter 4",
+#     "Lunar Orbiter 5",
+#     "Apollo 11",
+#     "Apollo 12",
+#     "Apollo 13",
+#     "Apollo 14",
+#     "Apollo 15",
+#     "Apollo 16",
+#     "Apollo 17",
+#     "Artemis",
+#     "Mangalyaan",
+#     "Hayabusa 2",
+#     "Beresheet",
+#     "OSIRIS-REx",
+#     "Tiangong 1",
+#     "Tiangong 2",
+#     "Tiangong 3",
+#     "Mir",
+#     "Skylab",
+#     "Tiangong 1",
+#     "Tiangong 2",
+#     "Tiangong 3"
+# ]
+
+nama_satelit = [
+    ["Hubble Space Telescope", 1990, "Teleskop Luar Angkasa", 11110],
+    ["International Space Station (ISS)", 1998, "Stasiun Luar Angkasa", 419725],
+    ["Tiangong Space Station", 2021, "Stasiun Luar Angkasa", 100000],
+    ["Vanguard 1", 1958, "Satelit Pengamat Bumi", 1.47],
+    ["Explorer 1", 1958, "Satelit Penelitian Ilmiah", 14.5],
+    ["Sputnik 1", 1957, "Satelit Komunikasi", 83.6],
+    ["Sputnik 2", 1957, "Satelit Penelitian Ilmiah", 508.3],
+    ["Telstar", 1962, "Satelit Komunikasi", 77],
+    ["Skynet 1", 1969, "Satelit Militer", 37],
+    ["Skynet 2", 1974, "Satelit Militer", 40],
+    ["Landsat 1", 1972, "Satelit Pengamat Bumi", 454],
+    ["Landsat 2", 1975, "Satelit Pengamat Bumi", 966],
+    ["Landsat 3", 1978, "Satelit Pengamat Bumi", 991],
+    ["Voyager 1", 1977, "Pesawat Luar Angkasa", 722],
+    ["Voyager 2", 1977, "Pesawat Luar Angkasa", 815],
+    ["Galileo", 1989, "Pesawat Luar Angkasa", 2544],
+    ["Chandra X-ray Observatory", 1999, "Teleskop Luar Angkasa", 5000],
+    ["Kepler Space Telescope", 2009, "Teleskop Luar Angkasa", 1050],
+    ["James Webb Space Telescope", 2022, "Teleskop Luar Angkasa", 6200],
+    ["Spitzer Space Telescope", 2003, "Teleskop Luar Angkasa", 950],
+    ["Compton Gamma Ray Observatory", 1991, "Teleskop Luar Angkasa", 17000],
+    ["Swift Gamma-Ray Burst Mission", 2004, "Teleskop Luar Angkasa", 1200],
+    ["Fermi Gamma-ray Space Telescope", 2008, "Teleskop Luar Angkasa", 4632],
+    ["Planck Space Observatory", 2009, "Teleskop Luar Angkasa", 1920],
+    ["Herschel Space Observatory", 2009, "Teleskop Luar Angkasa", 3300],
+    ["Gaia", 2013, "Teleskop Luar Angkasa", 2034],
+    ["Mars Reconnaissance Orbiter", 2005, "Pesawat Luar Angkasa", 2180],
+    ["Cassini-Huygens", 1997, "Pesawat Luar Angkasa", 5713],
+    ["New Horizons", 2006, "Pesawat Luar Angkasa", 478],
+    ["Juno", 2011, "Pesawat Luar Angkasa", 3625],
+    ["Chang'e 4", 2018, "Pesawat Luar Angkasa", None],
+    ["Chang'e 5", 2019, "Pesawat Luar Angkasa", None],
+    ["Chang'e 6", 2023, "Pesawat Luar Angkasa", None],
+    ["Lunar Reconnaissance Orbiter", 2009, "Pesawat Luar Angkasa", 1915],
+    ["Lunar Orbiter 1", 1959, "Pesawat Luar Angkasa", 385],
+    ["Lunar Orbiter 2", 1966, "Pesawat Luar Angkasa", 390],
+    ["Lunar Orbiter 3", 1967, "Pesawat Luar Angkasa", 390],
+    ["Lunar Orbiter 4", 1967, "Pesawat Luar Angkasa", 390],
+    ["Lunar Orbiter 5", 1967, "Pesawat Luar Angkasa", 390],
+    ["Apollo 11", 1969, "Pesawat Luar Angkasa", 28883],
+    ["Apollo 12", 1969, "Pesawat Luar Angkasa", 28883],
+    ["Apollo 13", 1970, "Pesawat Luar Angkasa", 28883],
+    ["Apollo 14", 1971, "Pesawat Luar Angkasa", 28883],
+    ["Apollo 15", 1971, "Pesawat Luar Angkasa", 28883],
+    ["Apollo 16", 1972, "Pesawat Luar Angkasa", 28883],
+    ["Apollo 17", 1972, "Pesawat Luar Angkasa", 28883],
+    ["Artemis", 2021, "Pesawat Luar Angkasa", None],
+    ["Mangalyaan", 2013, "Pesawat Luar Angkasa", None],
+    ["Hayabusa 2", 2014, "Pesawat Luar Angkasa", None],
+    ["Beresheet", 2019, "Pesawat Luar Angkasa", None],
+    ["OSIRIS-REx", 2016, "Pesawat Luar Angkasa", None],
+    ["Tiangong 1", 2011, "Stasiun Luar Angkasa", None],
+    ["Tiangong 2", 2016, "Stasiun Luar Angkasa", None],
+    ["Mir", 1986, "Stasiun Luar Angkasa", 129700],
+    ["Skylab", 1973, "Stasiun Luar Angkasa", 77400],
+]
+
+class Satelit:
+    def __init__(self, nama, tahun_peluncuran, jenis, massa):
+        self.nama = nama
+        self.tahun_peluncuran = tahun_peluncuran
+        self.jenis = jenis
+        self.massa = massa
+
+    def __str__(self):
+        return f"Satelit {self.nama} ({self.jenis}), diluncurkan pada tahun {self.tahun_peluncuran}, dengan massa {self.massa} kg"
+
+class Array(object):
+    def __init__(self) -> None:
+        self._internal = 10*[None]
+
+    def __getitem__(self, key):
+        return self._internal[key]
+    
+    def __setitem__(self, key, value):
+        if key >= len(self._internal):
+            kurang = key - len(self._internal) + 1
+            li = kurang * [None]
+            self._internal.extend(li)
+        self._internal[key] = value
+       
+array1 = Array()
+for index,satelit in enumerate(nama_satelit):
+    nama, tahun, jenis, massa = satelit[0], satelit[1], satelit[2], satelit[3]
+    stlit = Satelit(nama, tahun, jenis, massa)
+    array1[index] = stlit
+
+for i in array1:
+    print(i)
